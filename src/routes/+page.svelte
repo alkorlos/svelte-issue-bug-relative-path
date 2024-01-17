e7e34bd27383e354e50efcf9a3b2c1c127ebc013
Dmitry Statsenko <alkor.sx@gmail.com>
2024-01-18 00:22:53 +0300

    add font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	@font-face {
+		font-weight: normal;
+		font-family: "open-sans";
+		font-style: normal;
+		font-display: swap;
+		src: url("./fonts/OpenSans-Regular.woff2") format("woff2");
+	}
+</style>
